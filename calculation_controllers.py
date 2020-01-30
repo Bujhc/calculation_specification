5e33a74042a231e2a3c18a73fee04af001daff2f
Igor Margolin <igor.margolin@mail.ru>
2020-01-30 20:19:01 +0300

    modyfied some mistakes in calculate, choose signals

--- calculation_controllers.py
+++ calculation_controllers.py
@@ -1,6 +1,6 @@
 
 from itertools import combinations_with_replacement
-from sortedcontainers import SortedDict
+
 
 
 # calculation volume of the current signals of the programme
@@ -27,7 +27,7 @@ def modefideModulesController (modeles, type_of_signals):
     criteria = type_of_signals
     finish_spisok_modules = []
     for items in modeles:
-        if modules_controller[items][criteria] > 0:
+        if modules_controller[items][criteria] != 0:
             finish_spisok_modules.append(items)
     return finish_spisok_modules
 
@@ -37,19 +37,18 @@ def switch_brand(brand):
         1: "SH",
         2: "Carel"
     }
-    return brand
+    return brand[brand]
 
 def switch_type(type_of_signals_module):
-    type_of_signals_module = {
+    switch_type_of_signals = {
         0: "AI",
         1: "BI",
         2: "AO",
         3: "DO",
         4: "UI",
-        5: "CO",
-        6: "BO"
+        5: "CO"
     }
-    return type_of_signals_module
+    return switch_type_of_signals[type_of_signals_module]
 
 def index_max_SIGNAL_TASK(task):
     return task.index(max(task))
@@ -60,20 +59,24 @@ def index_max_SIGNAL_TASK(task):
 
 controller = {'MS-FAC3611-0': {'AI':8, 'BI':6, 'AO':6, 'BO':6, 'CO':0}
         }
-modules_controller = {'MS-IOM2721-0': {'AI':8, 'BI':0, 'AO':2, 'BO':0, 'CO':0, 'price':83},
-                    'MS-IOM3711-2': {'AI':4, 'BI':0, 'AO':0, 'BO':4, 'CO':4, 'price':154},
-                    'MS-IOM3721-0': {'AI':0, 'BI':16, 'AO':0, 'BO':0, 'CO':0, 'price':75},
-                    'MS-IOM3731-0': {'AI':0, 'BI':8, 'AO':0, 'BO':8, 'CO':0,'price':85},
-                    'MS-IOM4711-0': {'AI':6, 'BI':2, 'AO':2, 'BO':3, 'CO':4, 'price':127},
+modules_controller = {'MS-IOM3721-0': {'AI':0, 'BI':16, 'AO':0, 'BO':0, 'CO':0, 'price':75},
                     'MS-IOM1711-0': {'AI':0, 'BI':4, 'AO':0, 'BO':0, 'CO':0, 'price':77},
-                    'MS-IOM2711-2': {'AI':2, 'BI':0, 'AO':0, 'BO':2, 'CO':2, 'price':118}
+                    'MS-IOM2721-0': {'AI':8, 'BI':0, 'AO':2, 'BO':0, 'CO':0, 'price':83},
+                    'MS-IOM3731-0': {'AI':0, 'BI':8, 'AO':0, 'BO':8, 'CO':0,'price':85},
+                    'MS-IOM2711-2': {'AI':2, 'BI':0, 'AO':0, 'BO':2, 'CO':2, 'price':118},
+                    'MS-IOM4711-0': {'AI':6, 'BI':2, 'AO':2, 'BO':3, 'CO':4, 'price':127},
+                    'MS-IOM3711-2': {'AI':4, 'BI':0, 'AO':0, 'BO':4, 'CO':4, 'price':154}             
+                   
         }
-# task from operator
+# task from operator accordint next order AI BI AO DO UI CO
 
-SIGNAL_TASK = [10,11,5,7]
+SIGNAL_TASK = [20,25,35,47,49,65]
 
-print(index_max_SIGNAL_TASK(SIGNAL_TASK))
+cal_type_max_signal_task = index_max_SIGNAL_TASK(SIGNAL_TASK)
 
+type_of_signal = str(switch_type(cal_type_max_signal_task))
+
+print(type_of_signal)
 # clear of the list
 
 control_list = []
@@ -87,19 +90,20 @@ for i in controller:
     control_list.append(i)
 
 # combination for controller JC, 8 - max count of the modules connection to controller
-spisok = modefideModulesController(modules_controller, 'AI')
+spisok = modefideModulesController(modules_controller, type_of_signal)
 print(spisok)
+
+
 counter = 0
 rezult_by_signals = {}
-
 max_quality = 0
 
-for i in range(1,8): # how many modules you can use
+for i in range(1,9): # how many modules you can use
     for i, pair in enumerate(combinations_with_replacement(spisok, i), 1):
-        current_volume = calcBackVolume_signals(pair, 'AI')
+        current_volume = calcBackVolume_signals(pair, type_of_signal)
         current_cost = calcBackCost(pair)
         spisok_result =[]    
-        if current_volume <= SIGNAL_TASK[0]:
+        if current_volume <= SIGNAL_TASK[index_max_SIGNAL_TASK(SIGNAL_TASK)]:
             counter +=1
             spisok_result.append(current_cost)
             spisok_result.append(current_volume)
@@ -109,8 +113,8 @@ for i in range(1,8): # how many modules you can use
 
 dict_by_price = {}
 max_label = 0
-test_list_signals = []
-test_list_price = []
+list_signals = []
+
 # finish dictionary we filteres by signals from Task to reduce elements.
 
 counter_1 = 0
@@ -119,10 +123,15 @@ for i in rezult_by_signals:
         counter_1 +=1
         max_label = rezult_by_signals[i][1]
         dict_by_price.update({counter_1: rezult_by_signals[i][2]})
-        #print(rezult_by_signals[i][1])
-        test_list_signals.append(rezult_by_signals[i][1])
+        
+        list_signals.append(rezult_by_signals[i][1])
 
-index_in_sort_dictionary = test_list_signals.index(max(test_list_signals)) + 1       
+index_in_sort_dictionary = list_signals.index(max(list_signals)) + 1       
 
-print(dict_by_price[index_in_sort_dictionary])
+# finish list of modules after 1 step calculation
+rezult_1step = (dict_by_price[index_in_sort_dictionary])
+print(rezult_1step)
+
+
+
 
